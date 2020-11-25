4e042264015bc48b80eec39fbba8afcb90b54296
Dhiman Seal <furyx.ds@gmail.com>
2020-11-25 20:25:17 +0530

    Optimized imports

--- lib/UI/employeeManage/components/employeeAdd.dart
+++ lib/UI/employeeManage/components/employeeAdd.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/single_child_widget.dart';
 
 import 'package:safe_sync/Backend/bloc/databaseBloc.dart';
 import 'package:safe_sync/Backend/constants.dart';
